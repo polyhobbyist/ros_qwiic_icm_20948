f7a922c7860591fcc907eb024cf2df7d4f65d829
Lou Amadio <lou@polyhobbyist.com>
2022-06-29 15:37:55 -0700

    Update for submodules add message generation

--- src/main.cpp
+++ src/main.cpp
@@ -25,17 +25,33 @@ SOFTWARE.
 #include <memory>
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include <boost/asio.hpp>
-#include <std_msgs/msg/float32.hpp>
-#include <geometry_msgs/msg/twist.hpp>
+#include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/magnetic_field.hpp"
 
-#ifdef _WIN32
-#pragma optimize( "", off )
-#else
+#include "Arduino.h"
 #include "Wire.h"
 
-#pragma GCC optimize ("O0")
-#endif
+#include "ICM_20948.h"
+
+#define AD0_VAL 1      // The value of the last bit of the I2C address.                
+                       // On the SparkFun 9DoF IMU breakout the default is 1, and when 
+                       // the ADR jumper is closed the value becomes 0
+
+float MicroTeslaToTesla(float mT)
+{
+  return mT * 1000000;
+}
+
+float MiniGToMeterPerSecond(float g)
+{
+  return g * 1000 * 9.80665;
+}
+
+float DegreesPerSecondToRadsPerSecond(float dps)
+{
+  return dps * 57.295779578552;
+}
+
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -48,20 +64,60 @@ class I2CPublisher : public rclcpp::Node
     , _id(0) 
     {
       get_parameter_or<uint8_t>("id", _id, 0x5D); 
+      get_parameter_or<std::string>("frame_id", _frameId, "imu"); 
+      get_parameter_or<std::string>("topicImu", _topicImu, "/imu/data_raw"); 
+      get_parameter_or<std::string>("topicMag", _topicMag, "/imu/mag"); 
+      get_parameter_or<double>("poll", _poll, 15.0);
 
-      //_publisher = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      _timer = this->create_wall_timer(500ms, std::bind(&I2CPublisher::timer_callback, this));
+      myICM.begin(Wire, AD0_VAL);
+
+      _publisherImu = this->create_publisher<sensor_msgs::msg::Imu>(_topicImu, 10);
+      _publisherMag = this->create_publisher<sensor_msgs::msg::MagneticField>(_topicMag, 10);
+      _timer = this->create_wall_timer(
+        std::chrono::duration<double, std::milli>(_poll), 
+        std::bind(&I2CPublisher::timer_callback, this));
     }
 
   private:
     void timer_callback()
     {
-      //auto message = std_msgs::msg::String();
-      //_publisher->publish(message);
+      if (myICM.dataReady())
+      {
+        myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
+
+        auto messageMag = sensor_msgs::msg::MagneticField();
+        messageMag.header.frame_id = _frameId;
+        messageMag.header.stamp = rclcpp::Clock().now();
+        messageMag.magnetic_field.x = MicroTeslaToTesla(myICM.magX());
+        messageMag.magnetic_field.y = MicroTeslaToTesla(myICM.magY());
+        messageMag.magnetic_field.z = MicroTeslaToTesla(myICM.magZ());
+        _publisherMag->publish(messageMag);
+
+        auto messageImu = sensor_msgs::msg::Imu();
+        messageImu.header.frame_id = _frameId;
+        messageImu.header.stamp = rclcpp::Clock().now();
+        messageImu.linear_acceleration.x = MiniGToMeterPerSecond(myICM.accX());
+        messageImu.linear_acceleration.y = MiniGToMeterPerSecond(myICM.accY());
+        messageImu.linear_acceleration.z = MiniGToMeterPerSecond(myICM.accZ());
+
+        messageImu.angular_velocity.x = DegreesPerSecondToRadsPerSecond(myICM.gyrX());
+        messageImu.angular_velocity.y = DegreesPerSecondToRadsPerSecond(myICM.gyrY());
+        messageImu.angular_velocity.z = DegreesPerSecondToRadsPerSecond(myICM.gyrZ());
+
+        _publisherImu->publish(messageImu);
+      }
     }
+
+    ICM_20948_I2C myICM;
+
     rclcpp::TimerBase::SharedPtr _timer;
-    //rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr _publisherImu;
+    rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr _publisherMag;
     uint8_t _id;
+    double _poll;
+    std::string _topicImu;
+    std::string _topicMag;
+    std::string _frameId;
 
 };
 
@@ -71,6 +127,10 @@ int main(int argc, char * argv[])
 
     auto node = std::make_shared<I2CPublisher>();
     node->declare_parameter("i2c_address");
+    node->declare_parameter("frame_id");
+    node->declare_parameter("poll");
+    node->declare_parameter("topicImu");
+    node->declare_parameter("topicMag");
 
     rclcpp::spin(node);
     rclcpp::shutdown();
